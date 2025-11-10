dbf9f69c20866fb17dd388d571c62d6dad339caa
mmauriciobastos <mauriciofib@gmail.com>
2025-11-10 08:51:00 -0800

    feat: add logo and description on signin

--- src/app/(full-width-pages)/(auth)/signin/page.tsx
+++ src/app/(full-width-pages)/(auth)/signin/page.tsx
@@ -2,8 +2,8 @@ import SignInForm from "@/components/auth/SignInForm";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Next.js SignIn Page | TailAdmin - Next.js Dashboard Template",
-  description: "This is Next.js Signin Page TailAdmin Dashboard Template",
+  title: "SaaS - Fuel Delivery",
+  description: "Sign in to your account to manage fuel deliveries and orders.",
 };
 
 export default function SignIn() {
